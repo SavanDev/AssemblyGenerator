89819e5f09723b0ec2d7c5eb9ea6174ee2a607b9
SavanDev <Dylan2018.2019@gmail.com>
2020-09-27 02:17:20 -0300

    Integrate NuGet packages correctly

--- AssemblyGenerator/buildMerge.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-if not exist bin\Merged (
-    mkdir bin\Merged
-)
-
-Thirdparty\ILMerge\ILMerge.exe bin\Release\AssemblyGenerator.exe /lib:bin\Release /out:bin\Merged\AssemblyGenerator.exe Newtonsoft.Json.dll /target:exe
